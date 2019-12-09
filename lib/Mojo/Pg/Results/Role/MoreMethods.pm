0053b58033b6f8624129fe41042daf3867247524
Adam Hopkins <violapiratejunky@gmail.com>
2019-12-08 18:58:50 -0600

    Add wrapper roles for Mojo::Pg::Results and Mojo::mysql::Results

---
+++ lib/Mojo/Pg/Results/Role/MoreMethods.pm
@@ -0,0 +1,8 @@
+package Mojo::Pg::Results::Role::MoreMethods;
+use Mojo::Base -role;
+
+with 'Mojo::DB::Results::Role::MoreMethods';
+
+our $VERSION = '0.01';
+
+1;
---
+++ lib/Mojo/mysql/Results/Role/MoreMethods.pm
@@ -0,0 +1,8 @@
+package Mojo::mysql::Results::Role::MoreMethods;
+use Mojo::Base -role;
+
+with 'Mojo::DB::Results::Role::MoreMethods';
+
+our $VERSION = '0.01';
+
+1;
